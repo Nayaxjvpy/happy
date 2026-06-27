c6a27f6740247dc05cb139b83b4a5c9bbff2f117
Nayaxjvpy <nayax.gd.mc@gmail.com>
2026-06-27 14:41:23 -0600

    Add files via upload

---
+++ CdeC/interaccion.js
@@ -0,0 +1,8 @@
+const boton = document.getElementById("boton1");
+
+boton.addEventListener("click", ()=> {
+    window.location.href = "interior2.html";
+});
+boton.addEventListener("touchstart", ()=> {
+    window.location.href = "interior2.html";
+});
